999ebf314945d02580afdcf68581ede00310ef95
Matheus Monteiro <matheusyanmonteiro@gamil.com>
2021-03-09 22:20:10 -0300

    finished basic routes and concepts

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const customers = [];
 
-app.post("/account", (request,response) => {
+app.post("/account", (request, response) => {
   const {cpf, name} = request.body;
 
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
@@ -25,6 +25,18 @@ app.post("/account", (request,response) => {
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+  
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({error: "Customer not found! "});
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
